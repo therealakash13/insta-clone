f11ed34e3dd580eeec4b587850efa7d5d1d23a6e
Akash <therealakash13@gmail.com>
2024-08-28 17:56:56 +0530

    right sidebar

--- frontend/src/components/RightSidebar.jsx
+++ frontend/src/components/RightSidebar.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import SuggestedUsers from "./SuggestedUsers";
 
 export default function RightSidebar() {
-  return <div>RightSidebar</div>;
+  const { user } = useSelector((store) => store.auth);
+  return (
+    <Fragment>
+      <div className="my-10 w-fit pr-32">
+        <div className="flex items-center gap-2">
+          <Link to={`/profile/${user?._id}`}>
+            <Avatar>
+              <AvatarImage src={user?.profilePicture} />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </Link>
+          <div>
+            <Link to={`/profile/${user?._id}`}>
+              <h1 className="font-semibold text-sm">{user?.username}</h1>
+            </Link>
+            <span className="text-gray-600">{user?.bio || "Bio here..."}</span>
+          </div>
+        </div>
+        <SuggestedUsers />
+      </div>
+    </Fragment>
+  );
 }
---
+++ frontend/src/components/SuggestedUsers.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function SuggestedUsers() {
+  return (
+    <>
+      <div className="my-10">
+        <div className="flex items-center justify-between text-sm space-x-4">
+          <h1 className="font-semibold text-gray-600 ">Suggested for you</h1>
+          <span className="font-medium cursor-pointer">See All</span>
+        </div>
+      </div>
+    </>
+  );
+}
